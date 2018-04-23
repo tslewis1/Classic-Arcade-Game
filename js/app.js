0f7b67e7cbb201f19f0ed0270837afab6e0cfc4f
Taryn Lewis <tarynslewis@gmail.com>
2018-04-23 12:24:32 -0700

    Remove unnecessary comments

--- js/app.js
+++ js/app.js
@@ -1,22 +1,17 @@
 // Enemies our player must avoid
 class Enemy {
 	constructor() {
-	// The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+	// The image/sprite for our enemies
 		this.sprite = 'images/enemy-bug.png';
 		this.speed = Math.floor(Math.random() * 4) + 2;
 		this.x = 0;
 		this.y = (Math.floor(Math.random() * 3) + 1);
 	};
 
-	// Update the enemy's position, required method for game
+	// Update the enemy's position
 	// Parameter: dt, a time delta between ticks
 	update(dt) {
-	// You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     	this.x = this.x + this.speed * dt;
-    	// alert(this.x + " " + this.speed + " " + dt);
 
     	// Creates method to handle enemy and player collision. On collision, the game resets.
     	if ((Math.round(this.x) == player.x) && (this.y == player.y)) {
@@ -34,9 +29,6 @@ class Enemy {
 		ctx.drawImage(Resources.get(this.sprite), (this.x  + 0.5) * 101, (this.y - 0.5) * 83);
 	};
 };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 // Player class that must avoid the enemies
 class Player {
@@ -98,9 +90,6 @@ class Player {
 		ctx.drawImage(Resources.get(this.sprite), this.x * 101, (this.y - 0.5) * 83);
 	};
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 var player = new Player();
 
@@ -128,7 +117,7 @@ function endOfGame() {
 };
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 $(document).on("keyup", function(e) {
     var allowedKeys = {
         37: 'left',
@@ -148,4 +137,5 @@ $(document).on("click", "#no", function() {
 $(document).on("click", "#yes", function() {
 	location.reload(true);
 	$(document).on();
+	gameIsDone = false;
 });
